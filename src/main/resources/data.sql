43bebcb44be0e6b494750f453d3bdf0e92e42e3f
volunteerocks <66223142+aabit@users.noreply.github.com>
2020-07-06 13:25:20 -0500

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO 
+	TBL_VOLUNTEERS (vol_full_name, vol_location, vol_email) 
+VALUES
+  	('full name', 'location', 'howtodoinjava@gmail.com'),
+  	('full name', 'location', 'xyz@email.com');
