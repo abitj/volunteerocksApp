ddf41da4d72d396ed9394d5805f956677deaba8d
volunteerocks <66223142+aabit@users.noreply.github.com>
2020-07-23 10:57:59 -0500

    testing

---
+++ src/test/java/com/example/demo_addFunc/VolunteerControllerIntegrationTest.java
@@ -0,0 +1,105 @@
+package com.example.demo_addFunc;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpClientErrorException;
+
+import com.example.demo_addFunc.model.VolunteerEntity;
+import com.example.demo_addFunc.service.VolunteerService;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = VolunteerEntity.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class VolunteerControllerIntegrationTest {
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@LocalServerPort
+	private int port;
+
+	private String getRootUrl() {
+		return "http://localhost:" + port;
+	}
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+	@Test
+	public void testGetAllVolunteers() {
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/volunteers",
+				HttpMethod.GET, entity, String.class);
+		
+		assertNotNull(response.getBody());
+	}
+
+	@Test
+	public void testGetEmployeeById() {
+		VolunteerEntity volunteer = restTemplate.getForObject(getRootUrl() + "/volunteers/1", VolunteerEntity.class);
+		System.out.println(volunteer.getVolFullName());
+		assertNotNull(volunteer);
+	}
+
+	@Test
+	public void testCreateVolunteer() {
+		VolunteerEntity volunteer = new VolunteerEntity();
+		volunteer.setVolEmail("admin@gmail.com");
+		volunteer.setVolFullName("admin");
+		volunteer.setVolLocation("admin");
+
+		ResponseEntity<VolunteerEntity> postResponse = restTemplate.postForEntity(getRootUrl() + "/volunteers", volunteer, VolunteerEntity.class);
+		assertNotNull(postResponse);
+		assertNotNull(postResponse.getBody());
+	}
+
+	@Test
+	public void testUpdateVolunteer() {
+		int id = 1;
+		VolunteerEntity volunteer = restTemplate.getForObject(getRootUrl() + "/volunteers/" + id, VolunteerEntity.class);
+		volunteer.setVolFullName("admin1");
+		volunteer.setVolLocation("admin2");
+		
+	
+		
+
+		restTemplate.put(getRootUrl() + "/volunteers/" + id, volunteer);
+
+		VolunteerEntity updatedVolunteer = restTemplate.getForObject(getRootUrl() + "/volunteers/" + id, VolunteerEntity.class);
+		assertNotNull(updatedVolunteer);
+	}
+
+	@Test
+	public void testDeleteVolunteer() {
+		int id = 2;
+		VolunteerEntity volunteer = restTemplate.getForObject(getRootUrl() + "/volunteers/" + id, VolunteerEntity.class);
+		assertNotNull(volunteer);
+
+		restTemplate.delete(getRootUrl() + "/volunteers/" + id);
+
+		try {
+			volunteer = restTemplate.getForObject(getRootUrl() + "/volunteers/" + id, VolunteerEntity.class);
+		} catch (final HttpClientErrorException e) {
+			assertEquals(e.getStatusCode(), HttpStatus.NOT_FOUND);
+		}
+	}
+}
