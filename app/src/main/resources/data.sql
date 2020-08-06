2511399633f53daa06f2d5d60ee1ae6de6e8659e
aredondo <aredondo@Austins-MacBook-Pro.local>
2020-08-05 20:18:28 -0500

    Changed html pages

--- app/src/main/resources/data.sql
+++ app/src/main/resources/data.sql
@@ -9,10 +9,14 @@ INSERT INTO
 	TBL_ORGANIZATION (title, description, location, volunteers) 
 VALUES
   	('UNO', 'Campus Trash Cleanup', '68106', 120),
-  	('Goodwill', 'Canned Food Drive', '68106', 1000);	  
+  	('Goodwill', 'Canned Food Drive', '68164', 1000),
+	('Mutual of Omaha', 'Security Personnel', '68101', 3),
+	('Omaha Public Schools', 'School Supplies', '68175', 50);	  
 
 INSERT INTO 
 	TBL_OPPORTUNITIES (description, location, number_needed) 
 VALUES
   	('Campus Trash Cleanup', '68106', 120),
-  	('Canned Food Drive', '68106', 1000);	  	  	  
+  	('Canned Food Drive', '68164', 1000),
+	('Security Personnel', '68101', 3),
+  	('School Supplies', '68175', 50);	  	  	  
