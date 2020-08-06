318f6e52e102d5b5670390be875557796c7ee417
aredondo <aredondo@Austins-MacBook-Pro.local>
2020-08-05 19:47:37 -0500

    Changed data.sql to include information

--- app/src/main/resources/data.sql
+++ app/src/main/resources/data.sql
@@ -1,17 +1,18 @@
 INSERT INTO 
 	TBL_VOLUNTEERS (full_name, location, email) 
 VALUES
-  	('full name', 'location', 'howtodoinjava@gmail.com'),
-  	('full name', 'location', 'xyz@email.com');
+  	('Corey Prideaux', '68105', 'cprideaux@unomaha.edu'),
+	('Abit Jurkuch', '68106', 'ajurkuch@unomaha.edu'),
+  	('Austin Redondo', '68105', 'aredondo@unomaha.edu');
 
 INSERT INTO 
 	TBL_ORGANIZATION (title, description, location, volunteers) 
 VALUES
-  	('title', 'description', '12345', 120),
-  	('title', 'description', '54321', 210);	  
+  	('UNO', 'Campus Trash Cleanup', '68106', 120),
+  	('Goodwill', 'Canned Food Drive', '68106', 1000);	  
 
 INSERT INTO 
 	TBL_OPPORTUNITIES (description, location, number_needed) 
 VALUES
-  	('description1', 'location1', 10),
-  	('descripiton2', 'location2', 4000);	  	  	  
+  	('Campus Trash Cleanup', '68106', 120),
+  	('Canned Food Drive', '68106', 1000);	  	  	  
